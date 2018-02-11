1c44beafebe094a02339272b440925c9aef0e58b
Extradyn <extradyn@extradyn.com>
2018-02-10 21:45:18 -0500

    works

--- spectrum.py
+++ spectrum.py
@@ -1,4 +1,6 @@
 import cookielib
+import string
+from string import replace
 import json
 import urllib2
 import urllib
@@ -12,7 +14,7 @@ password=config.get('credentials','password')
 serverList=config.get('spectrum','servers').split(',')
 dburl=config.get('influxdb','db_url')
 dbuser=config.get('influxdb','db_user')
-dbuser=config.get('influxdb','db_password')
+dbpassword=config.get('influxdb','db_password')
 dbname=config.get('influxdb','db_name')
 
 values = {'j_username' : username,
@@ -42,11 +44,12 @@ def dump():
 
 def writedb(data):
     headers = {"Content-type": "application/x-www-form-urlencoded", "Accept": "text/plain"}
+    print dburl
+    print data
     conn=httplib.HTTPConnection(dburl)
-    conn.request('POST', '', data, headers)
-    pass
-
-writedb("CREATE DATABASE " + dbname)
+    conn.request('POST', '/write?db=' + dbname + '&u=' + dbuser + '&p=' + dbpassword, data, headers)
+    #respi=conn.getresponse()
+    #print respi.read()
 
 for server in serverList:
   print server
@@ -64,15 +67,21 @@ for server in serverList:
   jsonContentSS = json.loads(content.read())
   for storageSystem in jsonContentSS:
     print("Sys: %s, %s" % (storageSystem['id'], storageSystem['Name']))
+    insertion='storagesystem,id=' + storageSystem['id'] + ',name=' + storageSystem['Name'] + ' PhysicalAllocation=' + replace(storageSystem['Physical Allocation'], ',','')
+    writedb(insertion)
     url = 'https://' + server + ':9569/srm/REST/api/v1/StorageSystems/' + storageSystem['id'] + '/Pools'
     contentPOOLS=fetch(url)
     jsonContentPOOLS = json.loads(contentPOOLS.read())
     for storagePool in jsonContentPOOLS:
+        insertion='pool,id=' + storagePool['id'] + ',name=' + storagePool['Name'] + ',storagesystem=' + storagePool['Storage System'] + ' PhysicalAllocation=' + replace(storagePool['Physical Allocation'],',','')
+        writedb(insertion)
         print("\\-- Pool: %s, %s" % (storagePool['id'], storagePool['Name']))
     url = 'https://' + server + ':9569/srm/REST/api/v1/StorageSystems/' + storageSystem['id'] + '/Volumes'
     contentVols=fetch(url)
     jsonContentVols=json.loads(contentVols.read())
     for volum in jsonContentVols:
+        insertion='volume,id=' + volum['id'] + ',name=' + volum['Name'] + ',Hosts=' + volum['Hosts'] + ',storagesystem=' + volum['Storage System'] + ',pool=' + volum['Pool'] + ' capacity=' + replace(volum['Capacity'],',','') + ',allocatedspace=' + replace(volum['Allocated Space'],',','')
+        writedb(insertion)
         print("\\-- Volume: %s, %s (Pool: %s)" % (volum['id'], volum['Name'], volum['Pool']))
     
 	
