2e5254a6add96241b7c25fd297d183a32f47251c
Extradyn <extradyn@extradyn.com>
2018-02-12 21:13:04 -0500

    fixes (spaces, buffering, exception catching)

--- spectrum.py
+++ spectrum.py
@@ -1,3 +1,4 @@
+import time
 import cookielib
 import string
 from string import replace
@@ -6,6 +7,7 @@ import urllib2
 import urllib
 import ConfigParser
 import httplib
+timest=(repr(time.time()).replace(".","")+'0000000')[0:19]
 
 config=ConfigParser.RawConfigParser()
 config.read('spectrum.cfg')
@@ -19,6 +21,8 @@ dbname=config.get('influxdb','db_name')
 
 values = {'j_username' : username,
           'j_password' : password}
+
+writebuff=''
 
 data = urllib.urlencode(values)
 
@@ -42,14 +46,27 @@ def dump():
     for cookie in cookies:
         print cookie.name, cookie.value
 
-def writedb(data):
+def flushbuf():
+    global writebuff
     headers = {"Content-type": "application/x-www-form-urlencoded", "Accept": "text/plain"}
-    print dburl
+    try:
+       conn=httplib.HTTPConnection(dburl)
+       conn.request('POST', '/write?db=' + dbname + '&u=' + dbuser + '&p=' + dbpassword, writebuff, headers)
+       respi=conn.getresponse()
+       print respi.read()
+       conn.close()
+    except:
+       pass
+    writebuff='' 
+
+def writedb(data):
+    global writebuff
+    headers = {"Content-type": "application/x-www-form-urlencoded", "Accept": "text/plain"}
+    writebuff=writebuff + data  + ' ' + timest + '\n'
     print data
-    conn=httplib.HTTPConnection(dburl)
-    conn.request('POST', '/write?db=' + dbname + '&u=' + dbuser + '&p=' + dbpassword, data, headers)
-    #respi=conn.getresponse()
-    #print respi.read()
+    if len(writebuff) > 5000:
+       flushbuf()
+       
 
 for server in serverList:
   print server
@@ -67,21 +84,22 @@ for server in serverList:
   jsonContentSS = json.loads(content.read())
   for storageSystem in jsonContentSS:
     print("Sys: %s, %s" % (storageSystem['id'], storageSystem['Name']))
-    insertion='storagesystem,id=' + storageSystem['id'] + ',name=' + storageSystem['Name'] + ' PhysicalAllocation=' + replace(storageSystem['Physical Allocation'], ',','')
+    insertion='storagesystem,server=' + server + ',id=' + storageSystem['id'] + ',name=' + replace(storageSystem['Name'],' ','') + ' PhysicalAllocation=' + replace(storageSystem['Physical Allocation'], ',','')
     writedb(insertion)
     url = 'https://' + server + ':9569/srm/REST/api/v1/StorageSystems/' + storageSystem['id'] + '/Pools'
     contentPOOLS=fetch(url)
     jsonContentPOOLS = json.loads(contentPOOLS.read())
     for storagePool in jsonContentPOOLS:
-        insertion='pool,id=' + storagePool['id'] + ',name=' + storagePool['Name'] + ',storagesystem=' + storagePool['Storage System'] + ' PhysicalAllocation=' + replace(storagePool['Physical Allocation'],',','')
+        insertion='pool,server=' + server + ',id=' + storagePool['id'] + ',name=' + replace(storagePool['Name'],' ','') + ',storagesystem=' + replace(storagePool['Storage System'],' ','') + ' PhysicalAllocation=' + replace(storagePool['Physical Allocation'],',','')
         writedb(insertion)
         print("\\-- Pool: %s, %s" % (storagePool['id'], storagePool['Name']))
     url = 'https://' + server + ':9569/srm/REST/api/v1/StorageSystems/' + storageSystem['id'] + '/Volumes'
     contentVols=fetch(url)
     jsonContentVols=json.loads(contentVols.read())
     for volum in jsonContentVols:
-        insertion='volume,id=' + volum['id'] + ',name=' + volum['Name'] + ',Hosts=' + volum['Hosts'] + ',storagesystem=' + volum['Storage System'] + ',pool=' + volum['Pool'] + ' capacity=' + replace(volum['Capacity'],',','') + ',allocatedspace=' + replace(volum['Allocated Space'],',','')
+        insertion='volume,server=' + server + ',id=' + volum['id'] + ',name=' + replace(volum['Name'],' ','') + ',storagesystem=' + replace(volum['Storage System'],' ','') + ',pool=' + replace(volum['Pool'],' ','') + ' capacity=' + replace(volum['Capacity'],',','') + ',allocatedspace=' + replace(volum['Allocated Space'],',','')
         writedb(insertion)
         print("\\-- Volume: %s, %s (Pool: %s)" % (volum['id'], volum['Name'], volum['Pool']))
     
+flushbuf()
 	
